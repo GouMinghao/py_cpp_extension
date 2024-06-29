8b69f8af514a082f1cc34f3f94f080c6942594a9
GouMinghao <gmh2015@163.com>
2024-06-29 17:12:03 +0800

    ENH: update pybind11 test.py

--- pybind11/test.py
+++ pybind11/test.py
@@ -2,10 +2,15 @@ import pybind_add
 
 a = 2
 b = 3
-c = pybind_add.add(a, b)
-print(a, b, c)
+print("====\nBind a single function")
+c1 = pybind_add.add(a, b)
+print("{} + {} = {}".format(a, b, c1))
 
-adder = pybind_add.AddClass(5, 2)
-print(adder.add())
+print("====\nBind a class")
+adder = pybind_add.AddClass(a, b)
+c2 = adder.add()
+print("{} + {} = {}".format(a, b, c2))
 
-print(pybind_add.ext_add(a, b))
+print("====\nBind a ext lib")
+c3 = pybind_add.ext_add(a, b)
+print("{} + {} = {}".format(a, b, c3))
